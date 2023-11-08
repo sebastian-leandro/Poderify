937a5d8853e2798a0d44a82d1c3cb963d5e85eab
sebastian-leandro <sebas.officciall@gmail.com>
2023-11-08 13:54:26 -0300

    Changing the service cards

--- components/Services.jsx
+++ components/Services.jsx
@@ -11,7 +11,7 @@ const Services = () => {
       <div className="gradient-01 w-[250px] h-[250px] bottom-64 left-1"></div>
       <div className="gradient top-36 w-[250px] h-[250px]"></div>
       <h2 className="heading txt-shadow uppercase">Nuestros Servicios</h2>
-      <div className="w-full rounded-2xl lg:flex-nowrap flex-wrap justify-center   gap-12  h-full flex md:flex-row flex-col ">
+      <div className="w-full rounded-2xl lg:flex-nowrap flex-wrap items-center justify-center gap-12 h-full flex lg:flex-row flex-col ">
         {servicesRow.map(({ img, alt, title, paragraph }, i) => (
           <CardRowService
             img={img}
--- modules/CardRowService.jsx
+++ modules/CardRowService.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const CardRowService = ({ img, alt, title, paragraph }) => {
   return (
-    <div className="lg:w-1/2 w-full min-h-[512px] md:min-h-[384px] bg-tertiary rounded-2xl flex shadow-md shadow-zinc-950">
+    <div className="sm:w-full w-[250px] h-[320px] md:min-h-[384px] bg-tertiary rounded-2xl flex shadow-md shadow-zinc-950">
       <div className="flex h-full items-center md:flex-row flex-col justify-center gap-y-4 w-full">
         <div className="w-full h-full relative">
           <Image
