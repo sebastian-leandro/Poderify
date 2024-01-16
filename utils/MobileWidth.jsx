5071d32c81393f43b43f85ebce0a122dbcbf6f45
sebastian-leandro <sebas.officciall@gmail.com>
2024-01-16 12:52:18 -0300

    changing vh to dvh

--- utils/MobileWidth.jsx
+++ utils/MobileWidth.jsx
@@ -6,7 +6,6 @@ export function MobileWidth () {
 
   useEffect(() => {
     const handleWidth = () => window.innerWidth <= 1024 ? setIsMobile(true) : setIsMobile(false)
-    handleWidth()
 
     window.addEventListener('resize', handleWidth)
     return () => window.removeEventListener('resize', handleWidth)
